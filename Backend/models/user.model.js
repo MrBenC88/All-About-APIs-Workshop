3c841c5b3a9d649c4e36c1e0053c8c7e4221d827
Daniel Ryu <rujjiryu99@gmail.com>
2020-08-11 16:13:17 -0700

    make some cosmetic changes

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
@@ -19,6 +19,6 @@ const userSchema = new Schema(
   }
 );
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,10 @@ const app = require('./app');
 const config = require('./utils/config');
 const http = require('http');
 
+// initialize server to run express app
 const server = http.createServer(app);
+
+// boot up server
 server.listen(config.PORT, () => {
   console.log(`server running on port ${config.PORT}`);
 })
